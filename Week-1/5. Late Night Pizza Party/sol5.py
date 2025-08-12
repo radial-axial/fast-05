002cae4f5e404f00ba70c27143a1e955c99f4c6b
radial-axial <r6samal@gmail.com>
2025-08-13 00:32:15 +0530

    Sol5

---
+++ Week-1/5. Late Night Pizza Party/sol5.py
@@ -0,0 +1,10 @@
+def pizza_ways(n):
+    if n <= 2:
+        return n
+    a, b = 1, 2
+    for _ in range(3, n + 1):
+        a, b = b, a + b
+    return b
+
+n = int(input())
+print(pizza_ways(n))
